9a228e52966c3e555215a70bee25e43d914ac8ad
daniperr0 <danilopirela6@gmail.com>
2025-05-25 20:52:54 -0400

    Update page.tsx

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -17,7 +17,7 @@ const PortfolioPage = () => {
                         </span>
                 </h1>
 
-                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-3 ">
+                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4 ">
                     {dataPortfolio.map((data)=> (
                         <PortfolioBox key={data.id} data={data}/>
 
