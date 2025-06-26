3a885b4a31cb3a403371a7b0e36f6b5ceebb5496
daniperr0 <danilopirela6@gmail.com>
2025-06-26 08:46:09 -0400

    Update page.tsx

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -15,7 +15,7 @@ const ServicePage = () => {
                     </span>
                 </h1>
                     <p className="mb-3 text-xl text-gray-300">
-                        Ofrezco mis servicios de desarrollo FullStack especializados en la creación de sitios web y aplicaciones atractivas y funcionales Utilizando las ultimas tecnologias como React.js, Next.js, TypeSript, TailwindCSS,Python, Bootstrap y Laravel, sin obviar el uso de las fundamentales como lo son HTML, CSS y JavaScript.
+                        Ofrezco mis servicios de desarrollo FullStack especializados en la creación de sitios web y aplicaciones atractivas y funcionales Utilizando las ultimas tecnologias como React.js, Next.js, TypeSript, TailwindCSS, Python, Bootstrap y Laravel, sin obviar el uso de las fundamentales como lo son HTML, CSS y JavaScript.
                     </p>
                     <Link className=" px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65" href={"https://wa.me/qr/V5T7NKA2T6GXH1"}>Contacta conmigo</Link>
 
