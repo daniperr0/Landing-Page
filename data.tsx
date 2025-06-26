14734d9f003c43e530c3772707f6df612380226a
daniperr0 <danilopirela6@gmail.com>
2025-06-26 08:29:42 -0400

    Update data.tsx

--- data.tsx
+++ data.tsx
@@ -61,11 +61,4 @@ export const dataPortfolio = [
         urlGithub: "https://github.com/daniperr0/WebStore-Basic.git",
         urlDemo: "https://majestic-sherbet-6044ce.netlify.app",
     },
-    {
-        id: 3,
-        title: "Online Web Store",
-        image: "/image-3.png",
-        urlGithub: "https://github.com/daniperr0/WebStore-Basic.git",
-        urlDemo: "https://majestic-sherbet-6044ce.netlify.app",
-    }
 ];
